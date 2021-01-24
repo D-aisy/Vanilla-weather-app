151599af623fad91de5b83af642255e077363092
D-aisy <44583789+D-aisy@users.noreply.github.com>
2021-01-24 14:46:16 +0000

    Added current time via JavaScript

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
+function currentDate(timestamp) {
+    let date = new Date(timestamp)
+    let hours = date.getHours()
+        if (hours < 10) {
+            hours = `0${hours}`
+        }
+    let minutes = date.getMinutes()
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+    let day = days[date.getDay()]
+    return `${day} ${hours}:${minutes}`
+}
+
 function displayWeatherInformation(response) {
-console.log(response)
     document.querySelector("#city-element").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
     document.querySelector("#forecast").innerHTML = response.data.weather[0].description;
-    //document.querySelector("#precipitation").innerHTML = response.data.
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+    document.querySelector("#current-date").innerHTML = currentDate(response.data.dt * 1000)
 } 
 
 apiKey = "364a702f202dc863a32d2f7ebc07434e"
