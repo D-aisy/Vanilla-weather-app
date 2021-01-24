05d2497c220ce1854e0118b65a53e81384d913f6
D-aisy <44583789+D-aisy@users.noreply.github.com>
2021-01-24 16:46:07 +0000

    Changed weather icon in Javascript

--- src/app.js
+++ src/app.js
@@ -28,10 +28,12 @@ function displayWeatherInformation(response) {
     document.querySelector("#humidity").innerHTML = response.data.main.humidity;
     document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
     document.querySelector("#current-date").innerHTML = currentDate(response.data.dt * 1000)
+    document.querySelector("#weather-icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+    document.querySelector("#weather-icon").setAttribute("alt", response.data.weather[0].description)
 } 
 
 apiKey = "364a702f202dc863a32d2f7ebc07434e"
-city = "London"
+city = "Sao Paulo"
 units = "metric"
 apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
 axios.get(apiUrl).then(displayWeatherInformation);
